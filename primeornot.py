8eda2db74f6053b77122f667d65222bdc0a5d238
AmalenduBHareesh <144940805+AmalenduBHareesh@users.noreply.github.com>
2026-04-22 12:32:28 +0530

    Fix variable name in prime number check

--- primeornot.py
+++ primeornot.py
@@ -1,8 +1,8 @@
 # WAP to check wheather the given number is prime or not
 n=int(input())# take the user input
 if n>1: # if loop starts if n is greater than 1
-    for divisor in range(2,n//2+1):  # for loop starts with range starting with 1 to n/2+1
-        if n% divisor==0: # iteration starts by dividing the n by the divisor and checking wheather its 0
+    for i in range(2,n//2+1):  # for loop starts with range starting with 1 to n/2+1
+        if n% i==0: # iteration starts by dividing the n by the divisor and checking wheather its 0
             print('its not prime number') # print the o/p
             break # break the statement if the number is not prime
     else: # else the number is checked to be prime
